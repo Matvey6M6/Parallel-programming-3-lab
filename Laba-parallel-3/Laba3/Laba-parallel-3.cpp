86c8eb0f03f701dfc97068ec90e437d0d315bffc
Matvey6M6 <rolanddiskein666@mail.ru>
2024-05-15 20:55:44 +0300

    Fully completed 3 laboratory work and report
    - Mpi finished
    -Report completed

---
+++ Laba-parallel-3/Laba3/Laba-parallel-3.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <conio.h>
+#include <random>
+#include <chrono>
+#include <string>
+#include<windows.h>
+
+#include "mpi.h"
+
+using namespace std;
+
+#define MASTER_TO_SLAVE_TAG 1 
+#define SLAVE_TO_MASTER_TAG 4
+
+
+void FillMat(int n, int*** Mat) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			(*Mat)[i][j] = rand() % (100 + 100 + 1) - 100;
+		}
+	}
+}
+
+int** Reader(const std::string filename) {
+	std::fstream fin;
+	fin.open(filename);
+
+	int n;
+	fin >> n;
+
+	int** Mat = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		Mat[i] = new int[n];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			fin >> Mat[i][j];
+		}
+	}
+
+	return Mat;
+}
+
+void Writer(int n, int** Mat, const std::string filename)
+{
+	std::fstream fout;
+	fout.open(filename, std::ofstream::out | std::ofstream::trunc);
+	fout << n << std::endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			fout << Mat[i][j] << ' ';
+		}
+		fout << std::endl;
+	}
+}
+
+int** resize_matrix(int size)
+{
+	int** matrix = new int* [size];
+	for (int i = 0; i < size; ++i)
+		matrix[i] = new int[size];
+	return matrix;
+}
+
+int main(int argc, char* argv[]) {
+
+	srand(time(0));
+
+	CreateDirectory(L"res_matrix", NULL);
+	CreateDirectory(L"1_matrix", NULL);
+	CreateDirectory(L"2_matrix", NULL);
+	CreateDirectory(L"times", NULL);
+
+	int** Mat1 = NULL;
+	int** Mat2 = NULL;
+	int** Mat_res = NULL;
+
+	int rank, size;
+	double ti[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	std::fstream fout;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank == 0)
+	{
+		fout.open(std::string("times/time_thread_") + std::to_string(size) + std::string(".txt"), std::ofstream::out | std::ofstream::trunc);
+		std::cout << "size = " << size << endl;
+	}
+
+	for (int n = 200; n <= 2000; n += 200)
+	{
+		if (rank == 0)
+		{
+			
+			std::cout << n << std::endl;
+		}
+		Mat1 = resize_matrix(n);
+		Mat2 = resize_matrix(n);
+		Mat_res = resize_matrix(n);
+
+		MPI_Barrier(MPI_COMM_WORLD);
+		double wtime = MPI_Wtime();
+
+		for (int t = 1; t <= 10; t++)
+		{
+			if (rank == 0)
+			{
+				FillMat(n, &Mat1);
+				FillMat(n, &Mat2);
+				//std::cout << '\t' << i;
+			}
+			MPI_Bcast(&(Mat2[0][0]), n * n, MPI_INT, 0, MPI_COMM_WORLD);
+
+			int rows_per_process = n / size;
+			int rows_remaining = n % size;
+			int start_row = rank * rows_per_process + min(rank, rows_remaining);
+			int end_row = start_row + rows_per_process + (rank < rows_remaining ? 1 : 0);
+
+			for (int i = start_row; i < end_row; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					Mat_res[i][j] = 0;
+					for (int k = 0; k < n; k++)
+						Mat_res[i][j] += Mat2[i][k] * Mat1[k][j];
+				}
+			}
+
+			MPI_Barrier(MPI_COMM_WORLD);
+			if (rank == 0)
+				ti[t - 1] = MPI_Wtime() - wtime;
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		if (rank == 0)
+		{
+			Writer(n, Mat_res, std::string("res_matrix/res_matrix") + std::to_string(n) + std::string(".txt"));
+			Writer(n, Mat1, std::string("1_matrix/1_matrix") + std::to_string(n) + std::string(".txt"));
+			Writer(n, Mat2, std::string("2_matrix/2_matrix") + std::to_string(n) + std::string(".txt"));
+			fout << n << ';';
+			for (int i = 0; i < 10; i++)
+			{
+				fout << ti[i] << ';';
+				std::cout << ti[i] << ';';
+			}
+			fout << std::endl;
+			std::cout << std::endl;
+
+		}
+		for (int i = 0; i < n; i++)
+		{
+			delete[] Mat_res[i];
+			delete[] Mat1[i];
+			delete[] Mat2[i];
+		}
+
+		delete[] Mat_res;
+		delete[] Mat1;
+		delete[] Mat2;
+
+		Mat_res = NULL;
+		Mat1 = NULL;
+		Mat2 = NULL;
+
+	}
+
+	MPI_Finalize();
+	fout.close();
+
+
+	return 0;
+}
---
+++ Laba-parallel-3/check-3.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats as sts
+import statistics as stat
+import csv
+import random
+
+def reading(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+    mat = []
+    for l in lines:
+        mat.append(l.split('\n')[0])
+    for i in range(1, len(mat)):
+        mat[i] = mat[i].split(' ')
+    n = int(mat[0])
+    res_mat = np.zeros((n, n))
+    res_mat[0][0] = n
+    for i in range(n):
+        for j in range(n):
+            res_mat[i][j] = int(mat[i+1][j])
+    return res_mat
+
+def checking(mat1, mat2, mat_res, n):
+    check_mat = mat1.dot(mat2)
+    for i in range(n):
+        for j in range(n):
+            if check_mat[i][j] != mat_res[i][j]:
+                return False
+    return True
+
+
+def make_stat():
+    th = {1, 2, 4, 6, 8}
+    for threads_num in th:
+        with open(f'times/time_thread_{threads_num}.txt') as file:
+            lines = file.readlines()
+        times = []
+        #print(lines)
+        for l in lines:
+            times.append(l.split(';'))
+            
+        means = []
+        intervals = []
+        arr_n =[]
+        
+        for i in range(len(times)):
+            for j in range(len(times[i])):
+                if times[i][j] != '\n':
+                    times[i][j] = float(times[i][j])
+            times[i].remove('\n')
+            arr_n.append(int(times[i][0]))
+            times[i].pop(0)
+            
+            means.append(stat.mean(times[i]))
+            intervals.append(sts.t.interval(alpha = 0.95, df = len(times[i]) - 1, loc = stat.mean(times[i]), scale = sts.sem(times[i])))
+        
+        file1 = open(f'statistic/statistic_thread_{threads_num}.csv', 'w')
+        writer = csv.writer(file1, delimiter=';')
+        writer.writerow(["Кол-во элементов", "Среднее", "Интервал"])
+        for i in range(len(means)):
+            writer.writerow([arr_n[i], means[i], intervals[i]])
+
+        plt.plot(arr_n, means, marker="o")
+    plt.xlabel('num of elements')
+    plt.ylabel('time, milliseconds')
+        
+    plt.legend(['1 thread', '2 threads', '4 threads', '6 threads', '8 threads'])
+    plt.savefig('plot_times.png')
+    plt.show()
+    
+
+if __name__ == "__main__":
+    file = open('checking.txt', 'w')
+    make_stat()
+    for n in range(200, 2001, 200):
+        mat1 = reading(f'1_matrix/1_matrix{n}.txt')
+        mat2 = reading(f'2_matrix/2_matrix{n}.txt')
+        mat_res = reading(f'res_matrix/res_matrix{n}.txt')
+        res = checking(mat1, mat2, mat_res, n)
+        file.write(f'Cheling for n = {n}: {res}\n')
+        print(n, res)
+            
+        
+    
